3c26eb8e0bac1f0f3b6676ba5a70873ba1dff49f
Andikarya17 <andikaryaputra17@gmail.com>
2025-12-19 14:07:07 +0700

    feat: add EntrySiswaScreen composable with ViewModel integration

--- app/src/main/java/com/example/questapi_214/view/HalamanEntry.kt
+++ app/src/main/java/com/example/questapi_214/view/HalamanEntry.kt
@@ -1,2 +1,14 @@
 package com.example.questapi_214.view
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntrySiswaScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EntrySiswaViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+
+}
