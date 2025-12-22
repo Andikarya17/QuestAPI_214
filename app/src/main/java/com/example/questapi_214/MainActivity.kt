96b4d551592001bfcc9dd2825a646720c0dc8e6a
Andikarya17 <andikaryaputra17@gmail.com>
2025-12-22 13:58:05 +0700

    menambah strings.xml, HalamanEntry.kt, MainActivity.kt, SiswaTopAppBar.kt, EntryViewModel.kt

--- app/src/main/java/com/example/questapi_214/MainActivity.kt
+++ app/src/main/java/com/example/questapi_214/MainActivity.kt
@@ -4,14 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.questapi_214.ui.theme.QuestAPI_214Theme
+import com.example.questapi_214.view.uicontroller.DataSiswaApp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +14,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             QuestAPI_214Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                DataSiswaApp()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuestAPI_214Theme {
-        Greeting("Android")
     }
 }
