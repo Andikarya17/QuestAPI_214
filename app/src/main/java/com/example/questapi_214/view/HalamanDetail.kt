15a347b0ff56c871033dd32e9aa14f092bb4da4c
Andikarya17 <andikaryaputra17@gmail.com>
2025-12-25 11:06:42 +0700

    Menambahkan Scaffold dan TopAppBar pada halaman detail

--- app/src/main/java/com/example/questapi_214/view/HalamanDetail.kt
+++ app/src/main/java/com/example/questapi_214/view/HalamanDetail.kt
@@ -1,7 +1,9 @@
 package com.example.questapi_214.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.questapi_214.viewmodel.provider.PenyediaViewModel
@@ -15,4 +17,13 @@ fun HalamanDetail(
     viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
     val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(R.string.detail_siswa),
+                canNavigateBack = true,
+                navigateUp = navigateBack
+            )
+        },
+
 }
