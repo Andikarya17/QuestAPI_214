1cd3326dc15434d54b9b5fe2bb1b1ef05d5d4ad1
Andikarya17 <andikaryaputra17@gmail.com>
2025-12-25 11:05:44 +0700

    Menambahkan coroutine scope pada HalamanDetail

--- app/src/main/java/com/example/questapi_214/view/HalamanDetail.kt
+++ app/src/main/java/com/example/questapi_214/view/HalamanDetail.kt
@@ -2,6 +2,9 @@ package com.example.questapi_214.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.questapi_214.viewmodel.provider.PenyediaViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -10,4 +13,6 @@ fun HalamanDetail(
     navigateToEdit: (Int) -> Unit,
     modifier: Modifier = Modifier,
     viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
-)
+) {
+    val coroutineScope = rememberCoroutineScope()
+}
