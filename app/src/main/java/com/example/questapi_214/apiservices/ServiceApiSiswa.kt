7dc38f78ec87ec9799ea3c968ae50d1af96fafb3
Andikarya17 <andikaryaputra17@gmail.com>
2025-12-25 11:45:31 +0700

    menambah fungsi getSiswaById, editSiswa, dan hapusSiswa

--- app/src/main/java/com/example/questapi_214/apiservices/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_214/apiservices/ServiceApiSiswa.kt
@@ -3,17 +3,40 @@ package com.example.questapi_214.apiservices
 import com.example.questapi_214.modeldata.DataSiswa
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
 
 interface ServiceApiSiswa {
+
+    // GET ALL
     @GET("bacaTeman.php")
     suspend fun getSiswa(): List<DataSiswa>
 
+    // GET BY ID (DETAIL)
+    @GET("baca1teman.php")
+    suspend fun getSiswaById(
+        @Query("id") id: Int
+    ): DataSiswa
+
+    // INSERT
     @POST("insertTM.php")
     suspend fun postSiswa(
         @Body dataSiswa: DataSiswa
     ): Response<Void>
 
+    // UPDATE
+    @PUT("editTM.php")
+    suspend fun editSiswa(
+        @Query("id") id: Int,
+        @Body dataSiswa: DataSiswa
+    ): Response<Void>
 
+    // DELETE
+    @DELETE("deleteTM.php")
+    suspend fun hapusSiswa(
+        @Query("id") id: Int
+    ): Response<Void>
 }
