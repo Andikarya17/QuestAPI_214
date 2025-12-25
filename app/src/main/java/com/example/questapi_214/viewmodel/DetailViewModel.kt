d118ff6d485741683837d574c7bfc590e6e7bf66
Andikarya17 <andikaryaputra17@gmail.com>
2025-12-25 11:34:12 +0700

    membuat class DetailViewModel

--- app/src/main/java/com/example/questapi_214/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_214/viewmodel/DetailViewModel.kt
@@ -1,7 +1,16 @@
 package com.example.questapi_214.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
+import com.example.questapi_214.modeldata.DataSiswa
+import com.example.questapi_214.repositori.RepositoryDataSiswa
+
 sealed interface StatusUiDetail {
     data class Success(val siswa: DataSiswa) : StatusUiDetail
     object Error : StatusUiDetail
     object Loading : StatusUiDetail
 }
+
+class DetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryDataSiswa: RepositoryDataSiswa
+)
