1c7736df73b7e7edb5f8852ab147d4e68a7dc48b
Andikarya17 <andikaryaputra17@gmail.com>
2025-12-25 11:35:00 +0700

    membuat fungsi getSiswaById

--- app/src/main/java/com/example/questapi_214/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_214/viewmodel/DetailViewModel.kt
@@ -1,6 +1,10 @@
 package com.example.questapi_214.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.questapi_214.modeldata.DataSiswa
 import com.example.questapi_214.repositori.RepositoryDataSiswa
 
@@ -13,4 +17,14 @@ sealed interface StatusUiDetail {
 class DetailViewModel(
     savedStateHandle: SavedStateHandle,
     private val repositoryDataSiswa: RepositoryDataSiswa
-)
+) : ViewModel() {
+
+    var statusUiDetail: StatusUiDetail by mutableStateOf(StatusUiDetail.Loading)
+        private set
+
+    private val idSiswa: Int =
+        checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+
+    init {
+        getSiswaById()
+    }
